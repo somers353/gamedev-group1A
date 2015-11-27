bb6cf87caefd293f7cb388804e84d995b34f7fd5
David Kelly <hellodavidkelly@gmail.com>
2015-11-27 09:10:15 +0000

    adding changes to scene menu and davids level

---
+++ game_dev_1A/Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	/** Reference to image to be displayed*/
+	public Texture2D normalImage;
+	/** Reference to image to be displayed*/
+	public Texture2D rolloverImage;
+	
+	/** Method that manages when the mouse is hovered over the image*/	
+	private void OnMouseOver(){
+		GetComponent<GUITexture>().texture = rolloverImage;
+	}
+	/** Method that manages when the mouse leaves the image*/	
+	private void OnMouseExit(){
+		GetComponent<GUITexture>().texture = normalImage;		
+	}
+	/** Method that manages when the mouse clicks on the image*/	
+	private void OnMouseUp(){
+		Application.LoadLevel("scene4_david");
+	}
+}
---
+++ game_dev_1A/Assets/Scripts/Player.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float delayBetweenDeaths = 0.5f;
+	/** Reference to the amount of engine parts the player must collect on level 3*/
+	private int partsLeft = 5;
+	/** Reference to the amount of lives the player has on level 3*/
+	private int livesLeft = 3;
+
+	/** Reference to the audio clip file*/
+	public AudioClip Die;
+
+	/** Reference to the message that will appear*/
+	public string died = "(message to appear)";
+	
+	/** \return Returns the int value of the remaining Engine Parts on level 3*/
+	public int GetPartsLeft()
+	{
+		return partsLeft;
+	}
+	
+	/** \return Returns the lives remaining for the player on level 3*/
+	public int GetLivesLeft()
+	{
+		return livesLeft;
+	}
+	
+	/** 
+		Method that manages the items collided with by the referenced player
+	*/
+	private void OnTriggerEnter(Collider c)
+	{
+		string tag = c.tag;
+
+		if ("Water" == tag)
+		{
+			loseLife();
+			GetComponent<AudioSource>().PlayOneShot(Die);
+		}
+	}
+
+	/** Method that manages the re spawning of the player after he dies*/
+	private void MoveToStartPosition()
+	{
+		GameObject respawnGO = ChooseRandomObjectWithTag ("Respawn");
+		Vector3 startPosition = respawnGO.transform.position;
+		transform.position = startPosition;
+	}
+	
+	/** 
+		Method that finds the the game object with the string passed finds the 
+		length of the array of objects with the same tag
+		randomizes the selection and returns one of the game objects
+	
+	\param String with a tag word
+	\return tagged object at a random index
+	*/
+	private GameObject ChooseRandomObjectWithTag (string tag)
+	{
+		GameObject [] taggedObjects = GameObject.FindGameObjectsWithTag (tag);
+		int numTaggedObjects = taggedObjects.Length;
+		int randomIndex = Random.Range (0, numTaggedObjects);
+		return taggedObjects [randomIndex];
+	}
+	
+	/** 
+		Method that manages the lives of the player
+	<pre>
+	IF
+		livesLeft = 0
+		{
+			Game Over
+		}
+	</pre>
+	*/
+	private void loseLife()
+	{
+		MoveToStartPosition ();
+		livesLeft--;	
+		
+		if (0 == livesLeft)
+		{
+			//Application.LoadLevel();
+		}
+	}
+
+}
